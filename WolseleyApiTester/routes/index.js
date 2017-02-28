3a8e01e537cc9202e883933ef94c65b4877b861a
Verbium <verbium.timpayton@googlemail.com>
2017-02-28 10:55:29 +0000

    adding logging for header information on initial get call

--- WolseleyApiTester/routes/index.js
+++ WolseleyApiTester/routes/index.js
@@ -7,6 +7,7 @@ router.get('/wuk/cs/CognitoTab/v1/Shipments', function(req, res, next) {
     var xml = req.app.get('InvItem');
     var status = req.app.get('HTTPStatus');
     console.log('Get call being made, status code: '+status);
+    console.log('req header: '+req.header);
     switch (status){
         case '404':
             res.status(404).send('Not found');
